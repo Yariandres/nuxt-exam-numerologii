e8f0580ab1f225e7aec7dd783ac5c59bc26e2ff5
Yari Herrera <yari.andres@gmail.com>
2025-01-29 21:34:15 +0100

    updates packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@pinia/nuxt', '@prisma/nuxt'],
+  modules: [
+    '@nuxt/ui',
+    '@pinia/nuxt',
+    '@prisma/nuxt',
+    '@vueuse/nuxt',
+  ],
 });
