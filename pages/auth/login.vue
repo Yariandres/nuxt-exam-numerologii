b72333da77c9d27020d50da207bd0144b48a0dd9
Yari Herrera <yari.andres@gmail.com>
2025-01-29 15:12:53 +0100

    adds pages

---
+++ pages/auth/login.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+<template>
+  <section class="flex flex-col items-center justify-center h-screen">
+    <UCard>
+      <h1 class="text-2xl font-bold">login</h1>
+    </UCard>
+  </section>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="flex flex-col items-center justify-center h-screen">
+    <UCard>
+      <h1 class="text-2xl font-bold">Welcome to the Numerology Test!</h1>
+      <p class="mt-4">
+        You are about to take a test that will assess your knowledge of
+        numerology.
+      </p>
+      <h2 class="mt-6">Test Details:</h2>
+      <ul class="list-disc list-inside">
+        <li>Number of Questions: 30</li>
+        <li>Time Limit: 30 minutes</li>
+        <li>Passing Score: 50%</li>
+      </ul>
+      <h2 class="mt-6">Instructions:</h2>
+      <p>
+        Please read each question carefully and select the best answer. Good
+        luck!
+      </p>
+    </UCard>
+
+    <UButton class="mt-4">Start Test</UButton>
+  </section>
+</template>
