ac31f98b567c639f8250a63c542436356a49764f
Yari Herrera <yari.andres@gmail.com>
2025-02-03 16:35:09 +0100

    updates pages file

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,9 @@
-<script setup lang="ts">
-import questionsData from '~/composables/questionsData';
-const userStore = useUserStore();
-
-if (!userStore.user) {
-  navigateTo('/auth/login');
-}
-</script>
-
 <template>
-  <section class="flex flex-col items-center justify-center h-screen">
-    <UCard>
-      <p class="text-right">{{ userStore.user?.email }}</p>
-      <h1 class="text-2xl font-bold">Welcome to the Numerology Test!</h1>
-      <p class="mt-4">
-        You are about to take a test that will assess your knowledge of
-        numerology.
-      </p>
-      <h2 class="mt-6">Test Details:</h2>
-      <ul class="list-disc list-inside">
-        <li>Number of Questions: 30</li>
-        <li>Time Limit: 30 minutes</li>
-        <li>Passing Score: 50%</li>
-      </ul>
-      <h2 class="mt-6">Instructions:</h2>
-      <p>
-        Please read each question carefully and select the best answer. Good
-        luck!
-      </p>
-    </UCard>
-
-    <UButton
-      class="mt-4"
-      color="indigo"
-      size="lg"
-      :to="`/student/exam/${questionsData[0].slug}`"
-    >
-      Start Test
-    </UButton>
-  </section>
+  <div class="flex flex-col items-center justify-center h-screen">
+    <h1 class="text-2xl font-bold">Welcome to the Numerology Test!</h1>
+    <p class="mt-4">
+      You are about to take a test that will assess your knowledge of
+      numerology.
+    </p>
+  </div>
 </template>
--- pages/student/index.vue
+++ pages/student/index.vue
@@ -1,5 +1,46 @@
+<script setup lang="ts">
+const userStore = useUserStore();
+
+onMounted(() => {
+  if (!userStore.user) {
+    navigateTo('/auth/login');
+  }
+});
+
+definePageMeta({
+  layout: 'student',
+});
+</script>
+
 <template>
-  <div class="flex flex-col justify-center h-screen">
-    <h1>Student page</h1>
-  </div>
+  <section class="flex flex-col items-center justify-center h-screen">
+    <UCard>
+      <!-- <p class="text-right">{{ userStore.user?.email }}</p> -->
+      <h1 class="text-2xl font-bold">Welcome to the Numerology Test!</h1>
+      <p class="mt-4">
+        You are about to take a test that will assess your knowledge of
+        numerology.
+      </p>
+      <h2 class="mt-6">Test Details:</h2>
+      <ul class="list-disc list-inside">
+        <li>Number of Questions: 30</li>
+        <li>Time Limit: 30 minutes</li>
+        <li>Passing Score: 50%</li>
+      </ul>
+      <h2 class="mt-6">Instructions:</h2>
+      <p>
+        Please read each question carefully and select the best answer. Good
+        luck!
+      </p>
+    </UCard>
+
+    <UButton
+      class="mt-4"
+      color="indigo"
+      size="lg"
+      :to="`/student/${userStore.user?.name}/question/${questionsData[0].slug}`"
+    >
+      Start Test
+    </UButton>
+  </section>
 </template>
