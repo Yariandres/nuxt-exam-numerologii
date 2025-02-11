229c15da47ef382111d12b82ac80a1a8601f740c
Yari Herrera <yari.andres@gmail.com>
2025-02-11 09:56:14 +0100

    adds certificate endpoint

---
+++ server/utils/certificateStyles.ts
@@ -0,0 +1,25 @@
+export const certificateColors = {
+  primary: '#234E70',
+  secondary: '#666666',
+  text: '#333333',
+  border: '#234E70',
+};
+
+export const certificateFonts = {
+  title: {
+    size: 40,
+    font: 'Helvetica-Bold',
+  },
+  subtitle: {
+    size: 25,
+    font: 'Helvetica',
+  },
+  name: {
+    size: 30,
+    font: 'Helvetica-Bold',
+  },
+  body: {
+    size: 20,
+    font: 'Helvetica',
+  },
+};
