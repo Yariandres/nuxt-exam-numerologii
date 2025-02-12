ca8be2bfceb4df057d79c6e4676f6fc534dc3a23
Yari Herrera <yari.andres@gmail.com>
2025-02-12 16:21:15 +0100

    updates supabase config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@pinia/nuxt', '@vueuse/nuxt'],
+  modules: ['@nuxt/ui', '@pinia/nuxt', '@vueuse/nuxt', '@prisma/nuxt'],
+  runtimeConfig: {
+    // Private keys are only available on the server
+    // public keys are also exposed to the client
+    public: {
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseKey: process.env.SUPABASE_KEY,
+    },
+  },
 });
