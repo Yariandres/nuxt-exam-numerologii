3ec2b8355eaaa71886239fcfae5ec8314fac29e6
Yari Herrera <yari.andres@gmail.com>
2025-02-14 06:11:28 +0100

    updates layouts

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -1,8 +1,17 @@
 <template>
-  <div class="min-h-screen bg-gray-50">
+  <div class="min-h-screen flex flex-col">
     <AdminNavigation />
-    <main>
+    <main class="flex-1">
       <slot />
     </main>
+
+    <footer>
+      <div class="flex justify-center my-4">
+        <p class="text-gray-500 text-sm">
+          <span class="font-bold text-pink-300">&copy;</span>
+          {{ new Date().getFullYear() }}
+        </p>
+      </div>
+    </footer>
   </div>
 </template>
--- layouts/student.vue
+++ layouts/student.vue
@@ -1,13 +1,5 @@
 <template>
   <div class="min-h-screen flex flex-col">
-    <header class="ml-auto">
-      <nav
-        class="max-w-7xl mx-auto flex justify-between items-center py-4 px-4 sm:px-6 lg:px-8"
-      >
-        <StudentNavigation />
-      </nav>
-    </header>
-
     <main class="flex-1 max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8 py-8">
       <slot />
     </main>
@@ -19,9 +11,11 @@
           {{ new Date().getFullYear() }}
           <a
             href="https://www.linkedin.com/in/yari-herrera-9677a9160/"
-            class="hover:text-gray-200"
-            >Yari Herrera for
+            class="hover:text-gray-200 text-blue-300"
+            target="_blank"
+            >Yari Herrera
           </a>
+          <span>for </span>
           <a
             href="https://www.szkolanumerologii.pl/"
             class="hover:text-pink-500 text-pink-300 underline"
