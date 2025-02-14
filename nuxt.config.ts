7ff3ce6cbb53eb863965ad88ee0b8c7b128bdea4
Yari Herrera <yari.andres@gmail.com>
2025-02-14 13:26:44 +0100

    fresh

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,20 +25,10 @@ export default defineNuxtConfig({
     },
   },
   app: {
-    baseURL: '/',
     head: {
       link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
     },
-    cdnURL: process.env.NUXT_APP_CDN_URL || '',
-  },
-  ssr: true,
-  nitro: {
-    preset: 'netlify',
-  },
-  experimental: {
-    payloadExtraction: false,
-    renderJsonPayloads: true,
   },
 });
