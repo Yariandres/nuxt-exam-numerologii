689105ceaf137f95782766af17a4e86a65ba588f
Yari Herrera <yari.andres@gmail.com>
2025-02-16 22:48:47 +0100

    adds prisma/client and points to node_

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
     '@nuxt/ui',
     '@pinia/nuxt',
     '@vueuse/nuxt',
-    // '@nuxtjs/supabase',
+    '@nuxtjs/supabase',
     '@prisma/nuxt',
   ],
   supabase: {
