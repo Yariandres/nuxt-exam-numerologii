133259f39b05c2c515404053ae48e9e2cc544b57
Yari Herrera <yari.andres@gmail.com>
2025-02-21 11:45:10 +0100

    adds category composable

---
+++ composables/useCategories.ts
@@ -0,0 +1,21 @@
+export const useCategories = () => {
+  const categories = [
+    'Wstęp do numerologii ',
+    'Dzień urodzenia',
+    'Kryształ życia',
+    'Wibracje główne ',
+    'Podwibracje',
+    'Liczby magiczne',
+    'Liczby Mistrzowskie',
+    'Liczby wibracyjne',
+    'Wyznaczniki karmiczne',
+    'Horoskop',
+    'Biznes',
+    'Podpis',
+    'Numrologia Dziecięca',
+  ];
+
+  return {
+    categories,
+  };
+};
